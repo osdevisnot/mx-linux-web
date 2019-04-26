b92f4b0f98341655caffd8f67fdddf28506b2b14
osdevisnot <osdevisnot@gmail.com>
2019-04-26 11:20:45 -0700

    shell setup after install

--- install.sh
+++ install.sh
@@ -27,5 +27,5 @@ nvm install 'lts/*'
 npm install -g yarn prettier sort-package-json
 
 # Setup ZSH https://ohmyz.sh/
-chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh)
